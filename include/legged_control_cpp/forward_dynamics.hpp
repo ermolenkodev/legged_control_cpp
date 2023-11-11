70d90174ec0c759f2501ebd63ac110f001d6aca6
Nikita Ermolenko <nikita.ermolenko@jetbrains.com>
2023-11-11 22:34:04 +0200

    Implement forward dynamics for legged control
    The forward dynamics implementation, which includes the Composite Rigid Body Algorithms (CRBA) for computing mass matrix and non-linear effects, is added to the legged robot control library. Other helper functions to compute composite inertia and gravity effects, crucial to these computations, are also included.

---
+++ include/legged_control_cpp/forward_dynamics.hpp
@@ -0,0 +1,37 @@
+#ifndef LCC_FORWARD_DYNAMICS_H
+#define LCC_FORWARD_DYNAMICS_H
+
+#include "functional"
+#include "model.hpp"
+#include "type_aliases.hpp"
+
+namespace legged_ctrl {
+
+struct CRBAResult {
+  JointSpaceMatrix mass_matrix_;
+  VectorX nle_;
+
+  CRBAResult(JointSpaceMatrix mass_matrix, VectorX nle);
+};
+
+JointSpaceMatrix compute_mass_matrix_crba(MultibodyModel const &model,
+  std::vector<SpatialMatrix> const &Xup,
+  std::vector<SpatialVector> const &S,
+  std::vector<SpatialMatrix> const &Ic);
+
+CRBAResult crba(MultibodyModel const &model,
+  SystemConfiguration const &system_configuration,
+  ExternalForces const &external_forces = ExternalForces::none(),
+  Vector3 const &gravity = Vector3 { 0, 0, G });
+
+std::vector<SpatialMatrix> compute_composite_inertia(const MultibodyModel &model,
+  const std::vector<SpatialMatrix> &Xup);
+
+SpatialVector compute_gravity_effect(MultibodyModel const &model,
+  SystemConfiguration const &system_configuration,
+  ExternalForces const &external_forces = ExternalForces::none(),
+  Vector3 const &gravity = Vector3 { 0, 0, G });
+
+}// namespace legged_ctrl
+
+#endif// LCC_FORWARD_DYNAMICS_H
---
+++ src/legged_control_cpp/forward_dynamics.cpp
@@ -0,0 +1,82 @@
+#include "legged_control_cpp/forward_dynamics.hpp"
+#include "legged_control_cpp/inverse_dynamics.hpp"
+
+namespace legged_ctrl {
+
+CRBAResult::CRBAResult(JointSpaceMatrix mass_matrix, VectorX nle)
+  : mass_matrix_(std::move(mass_matrix)), nle_(std::move(nle))
+{}
+
+CRBAResult crba(MultibodyModel const &model,
+  SystemConfiguration const &system_configuration,
+  ExternalForces const &external_forces,
+  Vector3 const &gravity)
+{
+  VectorX const qdd = VectorX::Zero(model.num_bodies());
+  auto const &[V, A, F, Xup, S, C] = details::rnea_with_extended_output(
+    model, system_configuration.q, system_configuration.qd, qdd, false, external_forces, gravity);
+
+  std::vector<SpatialMatrix> const Ic = compute_composite_inertia(model, Xup);
+  JointSpaceMatrix const mass_matrix = compute_mass_matrix_crba(model, Xup, S, Ic);
+
+  return { mass_matrix, C };
+}
+
+#pragma clang diagnostic push
+#pragma GCC diagnostic push
+#pragma clang diagnostic ignored "-Wsign-conversion"
+#pragma GCC diagnostic ignored "-Wsign-conversion"
+
+std::vector<SpatialMatrix> compute_composite_inertia(const MultibodyModel &model, const std::vector<SpatialMatrix> &Xup)
+{
+  int const n_bodies = model.num_bodies();
+  std::vector<int> const &parent = model.get_parent_idxs();
+
+  std::vector<SpatialMatrix> Ic(model.I_);
+  for (int i = n_bodies - 1; i >= 0; --i) {
+    if (parent[i] != -1) { Ic[parent[i]] += Xup[i].transpose() * Ic[i] * Xup[i]; }
+  }
+
+  return Ic;
+}
+
+JointSpaceMatrix compute_mass_matrix_crba(MultibodyModel const &model,
+  std::vector<SpatialMatrix> const &Xup,
+  std::vector<SpatialVector> const &S,
+  std::vector<SpatialMatrix> const &Ic)
+{
+  int const n_bodies = model.num_bodies();
+  std::vector<int> const &parent = model.get_parent_idxs();
+
+  JointSpaceMatrix M = JointSpaceMatrix::Zero(n_bodies, n_bodies);
+  for (int i = 0; i < n_bodies; ++i) {
+    SpatialVector F = Ic[i] * S[i];
+    M(i, i) = S[i].transpose() * F;
+    int j = i;
+    while (parent[j] != -1) {
+      F = Xup[j].transpose() * F;
+      j = parent[j];
+      M(i, j) = S[j].transpose() * F;
+      M(j, i) = M(i, j);
+    }
+  }
+
+  return M;
+}
+
+SpatialVector compute_gravity_effect(const MultibodyModel &model,
+  const SystemConfiguration &system_configuration,
+  ExternalForces const &external_forces,
+  Vector3 const &gravity)
+{
+  VectorX const qdd = VectorX::Zero(model.num_bodies());
+  RNEAExtendedOutput const result = details::rnea_with_extended_output(
+    model, system_configuration.q, system_configuration.qd, qdd, true, external_forces, gravity);
+
+  return result.g();
+}
+
+#pragma clang diagnostic pop
+#pragma GCC diagnostic pop
+
+}// namespace legged_ctrl
