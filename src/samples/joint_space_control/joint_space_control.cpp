b438a40e4ce5233e81c69fb07169d33adef4937f
Nikita Ermolenko <nikita.ermolenko@jetbrains.com>
2024-01-14 19:01:41 +0200

    Finalize joint space control simulation and improve logging
    The simulation process now log joint positions in a file for subsequent visualisation.

--- src/samples/joint_space_control/joint_space_control.cpp
+++ src/samples/joint_space_control/joint_space_control.cpp
@@ -1,23 +1,26 @@
 #include "cmath"
+#include "legged_control_cpp/forward_dynamics.hpp"
+//#include "legged_control_cpp/jacobian.hpp"
 #include "legged_control_cpp/type_aliases.hpp"
 #include "legged_control_cpp/urdf.hpp"
-#include "legged_control_cpp/jacobian.hpp"
 #include "optional"
 #include <fstream>
 #include <iostream>
 #include <nlohmann/json.hpp>
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include <utility>
 
 using namespace legged_ctrl;
 
 using json = nlohmann::json;
 
-std::optional<json> read_config() noexcept
+std::optional<json> read_config(spdlog::logger &logger) noexcept
 {
   std::ifstream f("src/samples/joint_space_control/config.json");
   try {
     return json::parse(f);
   } catch (json::parse_error const &e) {
-    std::cerr << "Failed to parse config file:\n" << e.what() << '\n';
+    logger.error("Failed to parse config file: %s", e.what());
     return std::nullopt;
   }
 }
@@ -28,68 +31,117 @@ VectorX get_vector(json const &config, std::string const &key)
   return VectorX(Eigen::VectorXd::Map(vec.data(), static_cast<long>(vec.size())));
 }
 
-void simulate_joint_space_control(json const &config)
+Eigen::Matrix<double, SIX_DIM, SIX_DIM> get_matrix(json const &config, std::string const &key)
+{
+  std::vector<double> vec = config.at(key);
+  return Eigen::Matrix<double, SIX_DIM, SIX_DIM>(
+    Eigen::Matrix<double, SIX_DIM, SIX_DIM>::Map(vec.data(), SIX_DIM, SIX_DIM));
+}
+
+struct StateWithTimestamp
+{
+  double time_from_simulation_start_{};
+  VectorX state_{};
+
+  StateWithTimestamp(double timestamp, VectorX state) : time_from_simulation_start_(timestamp), state_(std::move(state))
+  {}
+
+  friend std::ostream &operator<<(std::ostream &os, StateWithTimestamp const &obj)
+  {
+    os << obj.time_from_simulation_start_ << ',' << obj.state_.transpose();
+    return os;
+  }
+};
+
+void write_trajectory_to_file(std::vector<StateWithTimestamp> const &trajectory, std::ofstream &file)
+{
+  for (auto const &sample : trajectory) { file << sample << '\n'; }
+}
+
+std::vector<StateWithTimestamp> simulate_joint_space_control(json const &config, spdlog::logger &logger)
 {
   VectorX const freq = get_vector(config, "freq");
   VectorX const two_pi_f = 2 * M_PI * freq;
   VectorX const amplitude = get_vector(config, "amp");
   VectorX const two_pi_f_amp = two_pi_f.array() * amplitude.array();
   // TODO fix this
-  VectorX const two_pi_f_squared_amp = two_pi_f.array() * two_pi_f_amp.array();
-  VectorX q0 = get_vector(config, "q0");
-  VectorX const q = q0;
-  VectorX const qd = get_vector(config, "qd0");
-  VectorX const qdd = get_vector(config, "qdd0");
+//  VectorX const two_pi_f_squared_amp = two_pi_f.array() * two_pi_f_amp.array();
+  VectorX const q0 = get_vector(config, "q0");
 
-  VectorX q_des = q;
-  VectorX qd_des = VectorX::Zero(q.size());
-  VectorX qdd_des = VectorX::Zero(q.size());
+  VectorX q = q0;
+  VectorX qd = get_vector(config, "qd0");
+  VectorX qdd;
+
+  VectorX q_des;
+  VectorX qd_des;
+//  VectorX qdd_des = VectorX::Zero(q.size());
 
   MultibodyModel const model = legged_ctrl::urdf_parser::parse_urdf("assets/ur5.urdf");
 
-  auto start_time = std::chrono::high_resolution_clock::now();
   VectorX const phi = get_vector(config, "phi");
   double time = 0.0;
 
   double const simulation_time = config.at("exp_duration");
-  int const dt = config.at("dt");
+  double const dt = config.at("dt");
+  int const dt_ms = static_cast<int>(dt * 1000);
+
+  VectorX tau;
+  auto const kp = get_matrix(config, "kp");
+  auto const kd = get_matrix(config, "kd");
+
+  std::vector<StateWithTimestamp> simulated_trajectory;
+  simulated_trajectory.reserve(static_cast<size_t>(simulation_time / dt));
 
   while (time < simulation_time) {
-    auto end_time = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> const elapsed_time = end_time - start_time;
-    time = elapsed_time.count();
-
     q_des = q0.array() + amplitude.array() * Eigen::sin(two_pi_f.array() * time + phi.array());
     qd_des = two_pi_f_amp.array() * Eigen::cos(two_pi_f.array() * time + phi.array());
-    qdd_des = two_pi_f_squared_amp.array() * -Eigen::sin(two_pi_f.array() * time + phi.array());
+//    qdd_des = two_pi_f_squared_amp.array() * -Eigen::sin(two_pi_f.array() * time + phi.array());
 
-    std::this_thread::sleep_for(std::chrono::milliseconds(dt));
+//    auto const J6 = compute_end_effector_frame_jacobian(model, q, LOCAL_WORLD_ALIGNED);
+//    auto const J = J6.topRows(3);
 
-    std::cout << "q_des: " << q_des.transpose() << '\n';
-    std::cout << "qd_des: " << qd_des.transpose() << '\n';
-    std::cout << "qdd_des: " << qdd_des.transpose() << "\n\n";
+    tau = kp * (q_des - q) + kd * (qd_des - qd);
 
-    auto J = compute_end_effector_frame_jacobian(model, q, LOCAL_WORLD_ALIGNED);
+    auto [M, C] = crba(model, SystemConfiguration{ q, qd }, ExternalForces{ VectorX::Zero(q.size()) });
+    auto M_inv = M.inverse();
 
-    std::cout << "J:\n" << J << '\n';
+    qdd = M_inv * (tau - C);
+    qd = qd + qdd * dt;
+    q = q + dt * qd + 0.5 * dt * dt * qdd; // NOLINT
+
+    simulated_trajectory.emplace_back(time, q);
+    time += dt;
+    std::this_thread::sleep_for(std::chrono::milliseconds(dt_ms));
   }
+
+  logger.info("Done");
+
+  return simulated_trajectory;
 }
 
 int main()
 {
-  auto optional_config = read_config();
+  auto logger = *spdlog::stdout_color_mt("lcc_joint_space_control");
+  logger.set_level(spdlog::level::debug);
+
+  auto optional_config = read_config(logger);
 
   if (!optional_config) {
-    std::cerr << "Failed to read config file" << '\n';
+    logger.error("Failed to read config file");
     return 1;
   }
 
+  std::vector<StateWithTimestamp> simulated_trajectory;
   try {
-    simulate_joint_space_control(*optional_config);
+    simulated_trajectory = simulate_joint_space_control(*optional_config, logger);
   } catch (...) {
-    std::cerr << "Failed to simulate joint space control" << '\n';
+    logger.error("Failed to simulate joint space control");
     return 1;
   }
+
+  std::ofstream outFile("joint_space_control.log");
+  write_trajectory_to_file(simulated_trajectory, outFile);
+  outFile.close();
 
   return 0;
 }
