9341ad7228c2689b32f79d41ae1f96baad11a5a8
Nikita Ermolenko <nikita.ermolenko@jetbrains.com>
2024-04-16 20:27:38 +0300

    Refactor code and handle multiple warnings. Disable GCC's -Wshadow.
    This commit includes code cleanup, handling of various warnings, and the disabling of the -Wshadow warning flag under GCC due to its strange warnings in constructors.

---
+++ src/samples/task_space_control/gains.hpp
@@ -0,0 +1,31 @@
+#ifndef LCC_GAINS_HPP
+#define LCC_GAINS_HPP
+
+#include "legged_control_cpp/type_aliases.hpp"
+
+namespace task_space_control
+{
+
+using namespace legged_ctrl;
+
+
+
+struct GainMatrices
+{
+  DiagonalMatrix Kp_pos;
+  DiagonalMatrix Kd_pos;
+  DiagonalMatrix Kp_orient;
+  DiagonalMatrix Kd_orient;
+
+  GainMatrices() : Kp_pos(3), Kd_pos(3), Kp_orient(3), Kd_orient(3)
+  {
+    Kp_pos.diagonal() = legged_ctrl::Vector3{ 500, 500, 500 };//NOLINT
+    Kd_pos.diagonal() = legged_ctrl::Vector3{ 50, 50, 50 };//NOLINT
+    Kp_orient.diagonal() = legged_ctrl::Vector3{ 10, 10, 10 };//NOLINT
+    Kd_orient.diagonal() = legged_ctrl::Vector3{ .1, .1, .1 };//NOLINT
+  }
+};
+
+}
+
+#endif
